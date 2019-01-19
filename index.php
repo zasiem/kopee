282dfa5c1abffb6a27984a55c99b93fe8ecf99bb
zasiem <36037512+zasiem@users.noreply.github.com>
2019-01-19 16:16:10 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,342 @@
+<!doctype html>
+<html lang="en">
+<head>
+
+  <link rel="icon" href="fav.png">
+
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+
+  <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+
+  <link rel="stylesheet" href="style.css">
+
+  <title>Kopee.id</title>
+
+</head>
+<body>
+
+  <nav class="navbar navbar-expand-lg navbar-dark fixed-top" style="position: absolute;">
+    <div class="container">
+      <a class="navbar-brand" href="#"><img src="logo.png" alt="" height="auto" width="100px;"></a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#layanan"><span>Layanan</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#mengapa"><span>Mengapa Kopee</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#stats"><span>Stastistik</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#tentang"><span>Tentang Kami</span></a>
+          </li>
+         <!--  <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownPortfolio" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Bahasa
+            </a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownPortfolio">
+              <a class="dropdown-item" href="#">Indonesia</a>
+              <a class="dropdown-item" href="#">Inggris</a>
+            </div>
+          </li>  -->
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+<div class="row">
+  <div class="col-xl-12">
+    <div class="header-text">
+      <h1 style="color: #6D5252;"><span style="color: #7D9E46">Solusi</span> Pertanian Kopi Indonesia</h1>
+      <p style="margin-top: 50px;">Kopee.id adalah Platform yang menghubungkan Pemodal - Koperasi - Pembeli (Luar atau Lokal) dengan tujuan untuk mensejahterahkan petani dan meningkatkan kualitas pertanian kopi Indonesia melalui koperasi digital.</p>
+      <div class="col-md-12">
+        <a href="#subs"><button type="button" class="btn btn-success" style="margin-left: 130px;">Daftar</button></a>
+        <a href="#subs"><button type="button" class="btn btn-outline-success">Masuk</button></a>
+      </div>
+    </div>
+    <div class="header-background">
+    </div>
+  </div>
+</div>
+
+<div class="row"> 
+  <div id="tentang" class="col-md-12" style="margin-bottom: 50px;">
+    <h2>Mengapa kami ada ?</h2>
+    <hr> 
+  </div>
+</div>
+
+<div class="row sejarah">
+
+  <div class="col-lg-6 cont" >
+    <img src="alasan1.png" alt="" style="height: 50px; width: 50px; float: left; margin:20px;">
+    <div class="sejarah-font">
+      <h3>Mensejahterahkan Petani</h3>
+      <p>Melalui Kopee.id kami ingin petani-petani kecil
+        dapat menjual kopinya dengan hasil yang lebih
+      tinggi melalui layanan kami. sehingga dapat meningkatkan kesejahteraan mereka. </p>
+    </div>
+  </div>
+
+  <div class="col-lg-6 cont" >
+    <img src="permodalan.svg" alt="" style="height: 50px; width: 50px; float: left; margin:20px;">
+    <div class="sejarah-font">
+      <h3>Permodalan Pertanian</h3>
+      <p>Petani-petani kecil tidak perlu lagi khawatir mengenai modal untuk biaya perawatan dan pengembangan lahannya. Melalui layanan kami mendapatkan modal jadi lebih mudah.</p>
+    </div>
+  </div>
+
+  <div class="col-lg-6 cont" >
+    <img src="Menghubungkan.svg" alt="" style="height: 50px; width: 50px; float: left; margin:20px;">
+    <div class="sejarah-font">
+      <h3>Menghubungkan ke Pasar Global</h3>
+      <p>Kebanyakan petani kopi bersandar pada pengepul dan tengkulak. Melalui layanan kami kini para petani memiliki pilihan lain dengan menjual langsung ke pasar global sehingga para petani dapat menjual hasil pertaniannya dengan harga yang layak dan juga tinggi.</p>
+    </div>
+  </div>
+
+  <div class="col-lg-6 cont" >
+    <img src="Pedampingan.svg" alt="" style="height: 50px; width: 50px; float: left; margin:20px;">
+    <div class="sejarah-font">
+      <h3>Pedampingan Petani</h3>
+      <p>Masih banyak petani yang belum menerapkan pengelolaan lahan dan tanaman kopi secara intensif yang menyebabkan kurangnya produktifitas lahan. Melalui layanan kami para petani akan mendapatkan bimbingan dari pedamping proffesional yang telah kami sediakan.</p>
+    </div>
+  </div>
+
+</div>
+
+<div class="row"> 
+  <div id="layanan" class="col-md-12" style="margin-bottom: 50px;">
+    <h2>Layanan</h2>
+    <hr> 
+  </div>
+</div>
+
+<div class="row layanan">
+
+  <div class="col-lg-4">
+    <div class="layanan-font">
+      <img src="KopeeInvest.svg" alt="" height="300">     
+      <p style="padding: 20px;">Investasikan uangmu di pertanian
+        kopi Indonesia. Dapatkan ROI hingga 
+      37%.</p>
+    </div>
+  </div>
+
+  <div class="col-lg-4">
+    <div class="layanan-font">
+      <img src="KopeeRasi.svg" alt="" height="300">     
+      <p style="padding: 20px;">Gabung menjadi Mitra lahan dan Petani
+        Kopee dan dapatkan keuntungan yang 
+      lebih.</p>
+    </div>
+  </div>
+
+  <div class="col-lg-4">
+    <div class="layanan-font">
+      <img src="KopeeLelang.svg" alt="" height="300">     
+      <p style="padding: 20px;">Dapatkan Kopi berkualitas baik biji kopi 
+        maupun kopi olahan dari petani-
+      petani terbaik. </p>
+    </div>
+  </div>
+
+</div>
+
+
+<div id="mengapa" class="reason">
+  <div class="row">
+    <div class="col-md-12" style="margin-bottom: 50px;">
+      <h2 style="color: white;">Mengapa Memilih Kopee</h2>
+      <hr style="background-color: white;"> 
+    </div>
+  </div>
+
+  <div class="row" style="padding: 0px 100px 50px 100px;">
+
+    <div class="col-lg-3">
+      <div class="reason-font">
+        <img src="memberidampak.svg" alt="" width="80">
+        <h4 style="padding: 20px; color: white;">Transaksi Transparan</h4>
+      </div>
+    </div>
+
+    <div class="col-lg-3">
+      <div class="reason-font">
+        <img src="aman.svg" alt="" width="80">
+        <h4 style="padding: 20px; color: white;">Aman dan Profesional</h4>
+      </div>
+    </div>
+
+    <div class="col-lg-3">
+      <div class="reason-font">
+        <img src="transaksi.svg" alt="" width="80">
+        <h4 style="padding: 20px; color: white;">Memberi Dampak Sosial</h4>
+      </div>
+    </div>
+
+    <div class="col-lg-3">
+      <div class="reason-font">
+        <img src="memilikinetwork.svg" alt="" width="80">
+        <h4 style="padding: 20px; color: white;">Memiliki Network yang Luas</h4>
+      </div>
+    </div>
+
+
+  </div>
+</div>
+
+
+<div id="stats" class="col-md-12" style="margin-bottom: 50px; margin-top: 50px;">
+  <h2>Statistik Kopee Saat Ini</h2>
+  <hr> 
+</div>
+
+<div class="statistik">
+  <div class="row" style="padding: 0px 100px;">
+
+    <div class="col-lg-3">
+      <div class="statistik-font">
+        <img src="1ha.svg" alt="" width="100%" height="100">
+
+        <p style="padding: 20px;">1 ha</p>
+      </div>
+    </div>
+
+    <div class="col-lg-3">
+      <div class="statistik-font">
+        <img src="1petani.svg" alt="" width="100%" height="100">
+
+        <p style="padding: 20px;">1 petani</p>
+      </div>
+    </div>
+
+    <div class="col-lg-3">
+      <div class="statistik-font">
+        <img src="2000an.svg" alt="" width="100%" height="100">
+
+        <p style="padding: 20px;">2000an tanaman kopi</p>
+      </div>
+    </div>
+
+    <div class="col-lg-3">
+      <div class="statistik-font">
+        <img src="1mitra.svg" alt="" width="100%" height="100">
+
+        <p style="padding: 20px;">1 mitra lahan</p>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+
+<div class="col-md-12" style="margin-bottom: 50px;">
+  <h2 style="text-align: left;">Gallery Lahan</h2>
+  <hr style="margin-left: 10px;"> 
+</div>
+
+<div class="row gallery">
+
+  <div class="col-lg-6" style="text-align: center; margin-right: -15px;">
+    <img src="gallery.png" alt="" height="100%" width="110%">     
+  </div>
+  <div class="col-lg-6" style="text-align: center;">
+    <img src="gallery1.jpg" alt="" height="100%" width="110%">     
+  </div>
+  <div class="col-lg-6" style="text-align: center; margin-right: -15px;">
+    <img src="gallery2.jpg" alt="" height="100%" width="110%">     
+  </div>
+  <div class="col-lg-6" style="text-align: center;">
+    <img src="gallery3.jpg" alt="" height="100%" width="110%">     
+  </div>
+
+</div>
+
+<div class="col-md-12" style="margin-bottom: 20px;">
+  <h2>Our Team</h2>
+  <hr> 
+</div>
+
+<div class="row team">
+
+  <div class="col-lg-3">
+    <div class="card" style="">
+      <img class="card-img-top" src="Rectangle 9.png" alt="foto" width="100%">
+      <div class="card-body" style="padding-bottom: 0px;">
+        <h3 class="card-title" style="margin-bottom: 2px;">Dimas Nashiruddin Al Faruq</h3>
+        <h4 style="color: #7D9E46; font-size: 16px">Founder & CEO</h4>
+        <p class="card-text" style="font-size: 13px;">A Digitalpreneur with Internet Marketing and launching business product  Experience. </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-lg-3">
+    <div class="card" style="">
+      <img class="card-img-top" src="eja.png" alt="foto" width="100%">
+      <div class="card-body" style="padding-bottom: 0px;">
+        <h3 class="card-title" style="margin-bottom: 2px;">Erza Putra Albasori</h3>
+        <h4 style="color: #7D9E46; font-size: 16px">Founder & CTO</h4>
+        <p class="card-text" style="font-size: 13px;">A Digitalpreneur with Internet Marketing and launching business product  Experience. </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-lg-3">
+    <div class="card" style="">
+      <img class="card-img-top" src="img.jpg" alt="foto" width="100%">
+      <div class="card-body" style="padding-bottom: 0px;">
+        <h3 class="card-title" style="margin-bottom: 2px;">Ahmad Sambang</h3>
+        <h4 style="color: #7D9E46; font-size: 16px">Founder & COO</h4>
+        <p class="card-text" style="font-size: 13px;">A Digitalpreneur with Internet Marketing and launching business product  Experience. </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-lg-3">
+    <div class="card" style="">
+      <img class="card-img-top" src="abc.png" alt="foto" width="100%">
+      <div class="card-body" style="padding-bottom: 0px;">
+        <h3 class="card-title" style="margin-bottom: 2px;">Rifky Auliada</h3>
+        <h4 style="color: #7D9E46; font-size: 16px">Co-Founder & Field Manager</h4>
+        <p class="card-text" style="font-size: 13px;">A Digitalpreneur with Internet Marketing and launching business product  Experience. </p>
+      </div>
+    </div>
+  </div>
+
+
+
+</div>
+
+<div id="subs" class="subs">
+  <div class="row">
+    <div class="col-md-12" style="margin-bottom: 20px;">
+      <h2 style="color: white; margin-bottom: 50px;">Ayo Dukung Pertanian<br>Kopi Indonesia. Menjadi Lebih Baik !</h2>
+      <p>Dapatkan notifikasi Kopee telah rilis melalui email anda.</p>
+      <input type="text" class="emailsubs" placeholder="masukkan email anda ...">
+      <button type="button" class="btn btn-warning">Subsribe</button>
+    </div>
+  </div>
+</div>
+
+<footer>
+  <p>2019 © KOPEE. All rights reserved. Crafted with <span style="color: brown;">♥</span> at Indonesia</p>
+</footer>
+
+<!-- Optional JavaScript -->
+<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+</body>
+</html>
